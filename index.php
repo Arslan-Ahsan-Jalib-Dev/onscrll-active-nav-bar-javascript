a2fb53db56de415cd618958db80725c897199268
Arslan Ahsan <arslanahsanjalib050@gmail.com>
2022-03-28 16:08:00 +0500

    index updated

---
+++ index.php
@@ -0,0 +1,96 @@
+    <div class="container"> 
+        <div class="row">
+            <div class="col-md-12">
+              <div class="wraper">
+                  <ul>
+                    <li class="active home">
+                      <a href="#">Home</a>  
+                    </li>
+                    <li class="about">
+                      <a href="#">About</a>  
+                    </li>
+                    <li class="testimonials">
+                      <a href="#">Testimonials</a>  
+                    </li>
+                    <li class="contact">
+                      <a href="#">Contact</a>  
+                    </li>
+                   <li class="footer">
+                      <a href="#">Footer</a>  
+                    </li>
+                    
+                    
+                  </ul>
+              </div>
+            </div>
+            <!--end col-->
+        </div>
+        <!--end row-->
+     
+    </div>
+
+    <section class="com_sec home" id="home">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+             <div class="Top">
+                <h2>
+                    Home
+                 </h2>
+             </div> 
+           </div>  
+        </div>  
+      </div>
+    </section>
+    <section class="com_sec about" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+             <div class="Top">
+                <h2>
+                    About
+                 </h2>
+             </div> 
+           </div>  
+        </div>  
+      </div>
+    </section>
+    <section class="com_sec testimonials" id="testimonials">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+             <div class="Top">
+                <h2>
+                    Testimonials
+                 </h2>
+             </div> 
+           </div>  
+        </div>  
+      </div>
+    </section>
+    <section class="com_sec contact" id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+             <div class="Top">
+                <h2>
+                    Contact
+                 </h2>
+             </div> 
+           </div>  
+        </div>  
+      </div>
+    </section>
+    <section class="com_sec footer" id="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+             <div class="Top">
+                <h2>
+                    Footer
+                 </h2>
+             </div> 
+           </div>  
+        </div>  
+      </div>
+    </section>
